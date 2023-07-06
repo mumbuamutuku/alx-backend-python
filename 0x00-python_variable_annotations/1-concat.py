3106a12f2a52dcdf663a4c0f22e9faec9f83dc09
mumbuamutuku <mumbuamutuku@gmail.com>
2023-07-06 19:42:41 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function concat that takes a string str1 and a string str2
+as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function concat
+    returns a concated string
+    """
+    return str1 + str2
