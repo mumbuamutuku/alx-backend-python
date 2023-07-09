0b9d6d2eebfd9bac50b08182b7d747b180d3b5bd
mumbuamutuku <mumbuamutuku@gmail.com>
2023-07-09 18:30:49 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function to_str that takes a float n as argument and
+returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function to_str
+    Args:
+    n (float)
+    Return
+    string rep of an argument
+    """
+    return str(n)
