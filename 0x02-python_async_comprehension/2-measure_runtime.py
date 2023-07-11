7a642cb1c4901b377a1d47bc07807b292c5ebabb
mumbuamutuku <mumbuamutuku@gmail.com>
2023-07-11 11:33:23 +0300

    all tasks

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Import async_comprehension from the previous file and write a
+measure_runtime coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather.
+
+measure_runtime should measure the total runtime and return it.
+
+Notice that the total runtime is roughly 10 seconds, explain it to yourself.
+"""
+
+import asyncio
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    measures the total runtime and return it.
+    """
+    start_time = asyncio.get_event_loop().time()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    endtime = asyncio.get_event_loop().time()
+    totaltime = endtime - start_time
+
+    return totaltime
