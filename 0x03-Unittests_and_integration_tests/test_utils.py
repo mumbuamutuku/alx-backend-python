aa366e177867af5b5d80c174d12d3addcc92a94c
mumbuamutuku <mumbuamutuku@gmail.com>
2023-07-31 14:46:36 +0300

    pycodestyle

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,9 @@ from parameterized import parameterized
 
 
 def access_nested_map(nested_map, path):
+    """
+    access nested method
+    """
     for key in path:
         if isinstance(nested_map, dict) and key in nested_map:
             nested_map = nested_map[key]
@@ -17,10 +20,16 @@ def access_nested_map(nested_map, path):
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    method to test that the method returns what it is supposed to.
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
         ])
     def test_access_nested_map(self, nested_map, path, results):
+        """
+        test with assertEqual that the function returns the expected result.
+        """
         self.assertEqual(access_nested_map(nested_map, path), results)
